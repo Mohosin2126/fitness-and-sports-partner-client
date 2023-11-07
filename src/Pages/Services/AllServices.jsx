cc76cabe22af89afec47ed9c8ce509ffc33cb9c8
Mohosin212601~ <mohosin2126@gmail.com>
2023-11-08 03:27:48 +0800

    all services data are showed

--- src/Pages/Services/AllServices.jsx
+++ src/Pages/Services/AllServices.jsx
@@ -8,11 +8,18 @@ const AllServices = ({allservice}) => {
     <img src={ServiceImage} className="w-96 h-56 rounded-lg shadow-2xl" />
     <div className=" bg-slate-800">
       <h1 className="text-xl  font-bold font-serif ml-4">{ServiceName}</h1>
-      <h4 className="text-red-400 ml-4">Body Builder</h4>
-      <p className="py-6 ml-4">{ServiceDescription}</p>
-
+    <div className="flex ">
+        {/* <img src={ServiceProvider.Name} alt="" /> */}
+    <h4 className="text-red-400 ml-4">{ServiceProvider.Name}</h4>
+    <h4 className="text-red-400 ml-4">{ServiceProvider.Name}</h4>
     </div>
+      <p className="py-6 ml-4">{ServiceDescription}</p>
+      <p className="ml-4">{ServicePrice}</p>
+      <button className="btn btn-outline btn-secondary btn-sm ml-4 ">View Detail</button>
+    </div>
+  
   </div>
+  
         </div>
     );
 };
