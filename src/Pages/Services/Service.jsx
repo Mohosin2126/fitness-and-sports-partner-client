2c3b9602c7bf4b60385a7939ae895cc0caa56c8d
Mohosin212601~ <mohosin2126@gmail.com>
2023-11-08 03:01:14 +0800

    service image added at popular service

--- src/Pages/Services/Service.jsx
+++ src/Pages/Services/Service.jsx
@@ -1,14 +1,14 @@
 
 
 const Service = ({service}) => {
-   const{id,Category,ServiceName,ServiceDescription,ServiceProvider,ServicePrice,ServiceArea}=service
+   const{id,Category,ServiceName,ServiceDescription,ServiceProvider,ServicePrice,ServiceArea,ServiceImage}=service
 
     return (
         <div >
             <div className="relative flex w-full h-96 flex-row rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
   <div className="relative w-2/5 m-0 overflow-hidden text-gray-700 bg-white rounded-r-none shrink-0 rounded-xl bg-clip-border">
     <img
-      src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1471&amp;q=80"
+      src={ServiceImage}
       alt="image"
       className="object-cover w-full h-full"
     />
