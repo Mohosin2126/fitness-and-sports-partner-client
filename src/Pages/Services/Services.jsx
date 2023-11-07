f190e167cbbdc8e86088f1409550e4ead3762dc4
Mohosin212601~ <mohosin2126@gmail.com>
2023-11-08 02:42:10 +0800

    changes the title

--- src/Pages/Services/Services.jsx
+++ src/Pages/Services/Services.jsx
@@ -13,7 +13,7 @@ const services=useLoaderData([])
   <div className="hero-overlay bg-opacity-50"></div>
   <div className="hero-content text-center text-neutral-content">
     <div className="max-w-md">
-      <h1 className="mb-5 text-2xl font-bold text-center ">I Grow By Helping People In Need</h1>
+      <h1 className="mb-5 text-2xl font-bold text-center ">Everything You Want All Are Here !</h1>
       <div className="flex ">
       <input type="text" placeholder="Type here" className="input input-bordered input-accent w-full max-w-xs " />
       <button className="btn btn-neutral bg-red-500 ">Search</button>
