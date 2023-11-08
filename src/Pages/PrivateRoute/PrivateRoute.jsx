37630f5f9d7dd34d1db84217d325942716acef67
Mohosin212601~ <mohosin2126@gmail.com>
2023-11-09 04:08:38 +0800

    private route done

--- src/Pages/PrivateRoute/PrivateRoute.jsx
+++ src/Pages/PrivateRoute/PrivateRoute.jsx
@@ -1,6 +1,7 @@
-import { useContext } from 'react';
-import { Navigate, useLocation } from 'react-router';
-import { AuthContext } from '../Providers/AuthProvider';
+import { useContext } from "react";
+import { AuthContext } from "../../Provider/AuthProvider/AuthProvider";
+import { Navigate, useLocation } from "react-router-dom";
+
 
 const PrivateRoute = ({children}) => {
     const {user, loading} = useContext(AuthContext);
