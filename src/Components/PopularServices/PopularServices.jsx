6e3d606652da1d91367f3fe7df2218de11b5ef7b
Mohosin212601~ <mohosin2126@gmail.com>
2023-11-12 15:22:19 +0800

    popular services modified

--- src/Components/PopularServices/PopularServices.jsx
+++ src/Components/PopularServices/PopularServices.jsx
@@ -2,11 +2,23 @@ import { useEffect } from "react";
 import { useState } from "react";
 import Service from "../../Pages/Services/Service";
 import { Link } from "react-router-dom";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const PopularServices = () => {
     const [services,setServices]=useState([])
+    useEffect(() => {
+        AOS.init({
+            duration: 1000,
+            delay: 100,
+            easing: 'ease-in-out',
+        });
+    }, []);
+
+
+
 useEffect(()=>{
-    fetch("http://localhost:5000/services")
+    fetch("https://fitness-and-sports-partner-server-8oww2jmsq.vercel.app/services")
     .then(res=>res.json())
     .then(data=>setServices(data))
 },[])
@@ -15,7 +27,7 @@ useEffect(()=>{
             <h1 className="text-2xl font-semibold font-serif text-center">Popular Services</h1>
             <p className="text-center font-medium mt-2">Explore the most sought-after fitness and sports services tailored to your specific goals, all in one convenient place</p>
         
-        <div className="grid md:grid-cols-2 gap-5 mt-10">
+        <div   className="grid md:grid-cols-2 gap-5 mt-10">
             {
                 services.slice(7).map(service=><Service service={service} key={service._id}></Service>)
             }
